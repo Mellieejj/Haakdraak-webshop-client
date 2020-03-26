915c7cf445457920696326aabdb9b9bba5503e5e
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-26 16:16:43 +0100

    checkout form made

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -2,6 +2,73 @@ import React, { Component } from "react";
 
 export default class CheckoutForm extends Component {
   render() {
-    return <div className="box">Hello checkoutForm</div>;
+    return (
+      <div className="box">
+        <h3>Bestel Formulier</h3>
+        <div>
+          <form>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Voornaam:</td>
+                  <td>
+                    <input type="text" name="firstName" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Achternaam:</td>
+                  <td>
+                    <input type="text" name="lastName" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Email:</td>
+                  <td>
+                    <input type="text" name="email" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Straat:</td>
+                  <td>
+                    <input type="text" name="straat" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Huisnummer:</td>
+                  <td>
+                    <input type="number" name="huisnr" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Postcode:</td>
+                  <td>
+                    <input
+                      type="text"
+                      name="postcode"
+                      required
+                      pattern="[0-9]{4}[A_Z]{2}"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Woonplaats:</td>
+                  <td>
+                    <input type="text" name="city" />
+                  </td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td></td>
+                  <td>
+                    <button>Bestel</button> <button>Wis alles</button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
