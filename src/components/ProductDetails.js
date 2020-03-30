f26c57c8bde52863ebbf2ffddbccd5d6657b7ae1
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-30 14:24:32 +0200

    image slider button styled, organized p order

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -57,54 +57,73 @@ export default class ProductDetails extends Component {
                   this.state.slideCount
                 ) {
                   return (
-                    <div key={image.id}>
+                    <div style={{ textAlign: "center" }} key={image.id}>
                       <img src={image.url} alt="" />
+                      {this.state.slideCount !== 0 ? (
+                        <BackArrow previousImage={this.previousImage} />
+                      ) : (
+                        ""
+                      )}
+                      {this.props.product.images ? (
+                        this.state.slideCount !==
+                        this.props.product.images.length - 1 ? (
+                          <NextArrow nextImage={this.nextImage} />
+                        ) : null
+                      ) : null}
                     </div>
                   );
                 }
                 return "";
               })
             : null}
-          {this.state.slideCount !== 0 ? (
-            <BackArrow previousImage={this.previousImage} />
-          ) : (
-            ""
-          )}
-          {this.props.product.images ? (
-            this.state.slideCount !== this.props.product.images.length - 1 ? (
-              <NextArrow nextImage={this.nextImage} />
-            ) : null
-          ) : null}
+
+          <p>
+            <span className="kopjes">Beschrijving:</span>
+            <br /> {this.props.product.description}
+          </p>
+
+          <p>
+            <span className="kopjes"> Optionele variaties:</span> <br />
+            {this.props.product.optioneel}
+          </p>
 
           <p>
             € {this.props.product.price}{" "}
             <i
               id="addToCart"
+              style={{ float: "center", marginLeft: "150px" }}
               onClick={() => this.props.clickHandler(this.props.product.id)}
-              className="fas fa-cart-plus fa-lg"
-            ></i>
+              className="fas fa-cart-plus"
+            >
+              {" "}
+              <span style={{ fontFamily: "Patrick Hand", fontSize: "20px" }}>
+                Toevoegen aan winkelwagentje
+              </span>
+            </i>
           </p>
-          <p>Op voorraad: {this.props.product.stock}</p>
+
+          <p>
+            <span className="kopjes">Op voorraad:</span>{" "}
+            {this.props.product.stock}
+          </p>
+
           {this.props.product.stock === 0 ? (
             <p>
-              Levertijd: <br />
+              <span className="kopjes">Levertijd:</span> <br />
               Producten die hier op de site staan, maak ik graag op bestelling.
               De levertijd is ongeveer 2 weken. De producten die je hier op de
               site vindt, zijn voorbeelden die ik al een keer gemaakt heb. Dit
               kan allemaal aangepast worden aan je eigen voorkeuren en ideeën.{" "}
             </p>
           ) : null}
-          <p>Formaat: +/- {this.props.product.size}</p>
+
           <p>
-            Beschrijving:
-            <br /> {this.props.product.description}
+            <span className="kopjes">Formaat:</span> +/-{" "}
+            {this.props.product.size}
           </p>
+
           <p>
-            Optionele variaties: <br />
-            {this.props.product.optioneel}
-          </p>
-          <p>
-            Categorie:{" "}
+            <span className="kopjes"> Categorie:</span>{" "}
             {this.props.product.categorie
               ? this.props.product.categorie.name
               : null}
