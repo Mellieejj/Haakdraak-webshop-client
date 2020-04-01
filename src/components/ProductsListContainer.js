6999a4fa2381161fd549f42b6c67e3cbf48ffff2
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-01 16:51:42 +0200

    made select field for the filter option

--- src/components/ProductsListContainer.js
+++ src/components/ProductsListContainer.js
@@ -5,6 +5,10 @@ import { getProducts, cartAdd } from "../actions/productActions";
 import ProductBox from "./ProductBox";
 
 class ProductListContainer extends Component {
+  state = {
+    show: "all"
+  };
+
   componentDidMount() {
     this.props.getProducts();
   }
@@ -12,13 +16,31 @@ class ProductListContainer extends Component {
   clickHandler = id => {
     this.props.cartAdd(id);
   };
-
+  filter = value => {
+    this.setState({
+      show: value
+    });
+  };
   render() {
     return (
       <div>
         <section className="top-product">
           <div className="productnaam">
             <h2>Producten</h2>
+          </div>
+          <div className="filter">
+            <label for="categorie">Filter producten:</label>
+
+            <select id="categorie" onChange={() => this.filter}>
+              <option value="all">Alle Producten</option>
+              <option value={"Bijtringen & Rammelaars"}>
+                {"Bijtringen & Rammelaars"}
+              </option>
+              <option value="Baby Overige">Baby Overige</option>
+              <option value="Knuffels">Knuffels</option>
+              <option value="Sleutelhangers">Sleutelhangers</option>
+              <option value="Sleutelhangers">Overige</option>
+            </select>
           </div>
           <ProductBox
             products={this.props.products}
