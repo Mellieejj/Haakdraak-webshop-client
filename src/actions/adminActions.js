2164914935de527e2d497e98a22f3a023f0bd62c
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-03 16:37:57 +0200

    made login action, login user reducer, login form, login is working

---
+++ src/actions/adminActions.js
@@ -0,0 +1,29 @@
+import request from "superagent";
+import { displayError } from "./orderActions";
+const baseUrl = "http://localhost:4000";
+// const baseUrl = "https://cherry-crumble-89582.herokuapp.com"
+
+export const JWT = "JWT";
+
+const loginUser = payload => {
+  return {
+    type: JWT,
+    payload
+  };
+};
+
+export function login(data) {
+  return async function(dispatch) {
+    try {
+      const response = await request.post(`${baseUrl}/login`).send(data);
+      const action = loginUser(response.body);
+      await dispatch(action);
+    } catch (error) {
+      if (error.response) {
+        const errorMessage = displayError(error.response.body.message);
+        return dispatch(errorMessage);
+      }
+      console.log(error);
+    }
+  };
+}
---
+++ src/components/adminComponents/LoginForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.onSubmit}>
+          <input
+            placeholder="name"
+            type="text"
+            value={this.props.values.name}
+            onChange={this.props.onChange}
+            name="name"
+          ></input>
+          <input
+            placeholder="password"
+            type="password"
+            value={this.props.values.password}
+            onChange={this.props.onChange}
+            name="password"
+          ></input>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
