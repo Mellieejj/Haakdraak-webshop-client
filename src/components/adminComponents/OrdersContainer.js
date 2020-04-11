b92baec4b1f0ad35f4a0a7e3c5ac7995f632c039
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-11 21:39:06 +0200

    made orders page component, showing after login

---
+++ src/components/adminComponents/OrdersContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class OrdersContainers extends Component {
+  render() {
+    return (
+      <div>
+        <div className="pagina-naam">
+          <h2>Alle bestellingen</h2>
+        </div>
+        <div>Lijst van bestellingen</div>
+      </div>
+    );
+  }
+}
